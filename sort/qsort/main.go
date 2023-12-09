ce1df855f1f5a9256c44c9e5fd33008d8835f0bc
Lev Orlov <lev.orlov.dev@gmail.com>
2023-12-09 19:20:59 -0300

    Add qsort

---
+++ sort/qsort/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	a := []int{9, 5, 2, 3, 5, 1, -1}
+	fmt.Println(a)
+	fmt.Println(qsort(a))
+}
+
+func qsort(a []int) []int {
+	if len(a) < 2 {
+		return a
+	}
+
+	left, right := 0, len(a)-1
+
+	// Pick a pivot
+	pivotIndex := rand.Int() % len(a)
+
+	// Move the pivot to the right
+	a[pivotIndex], a[right] = a[right], a[pivotIndex]
+
+	// Pile elements smaller than the pivot on the left
+	for i := range a {
+		if a[i] < a[right] {
+			a[i], a[left] = a[left], a[i]
+			left++
+		}
+	}
+
+	// Place the pivot after the last smaller element
+	a[left], a[right] = a[right], a[left]
+
+	// Go down the rabbit hole
+	qsort(a[:left])
+	qsort(a[left+1:])
+
+	return a
+}
